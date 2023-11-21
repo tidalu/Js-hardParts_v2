f2d7fd90299eb9d4c93eea799e4cbfdf8ac624fb
Tidalu <ul0271069@edu.uni.lodz.pl>
2023-11-21 20:06:30 +0100

    thread of executions

---
+++ JavascriptPrinciples1/ThreadsOfExecution1.js
@@ -0,0 +1,11 @@
+// when js code rund , it :
+
+// goes line by line and runs/ executes each line of known as the thread execution
+// saves data like strings and arrays so we can use that data later in its memory
+// We can even save code (‘functions’)
+
+// function 
+
+// Code we save (‘define’) functions &
+// can use (call/invoke/execute/run)
+// later with the function’s name & ( )
