3d2aa62433e38d571a5772fc643fdee7245eaf4e
Tidalu <ul0271069@edu.uni.lodz.pl>
2023-11-22 23:10:57 +0100

    learnt asnycronous in the backgroud

---
+++ Asyncronous javascript 4/single thread execution review.js
@@ -0,0 +1,15 @@
+ // asyncronous browser features
+
+//  dev tools
+// console -> console.
+// sockets
+// network request -> xhr/ fetch
+// html DOM -> document
+// timer -> setTimeout 
+
+
+// http://csbin.io/async
+// http://csbin.io/promises 
+// http://csbin.io/functional
+// http://csbin.io/oop
+// answers : https://github.com/FrontendMasters/fm-snippets/tree/main/javascript-hard-parts-v2
